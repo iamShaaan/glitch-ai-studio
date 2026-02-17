ca0dd0a00c89c773793be465aca24b3902ada466
Soumitro Halder Shan <shan@up-seo.at>
2026-02-17 16:05:48 +0600

    fix: remove CTA section from about page

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,11 +1,9 @@
 import { AboutSection } from "@/components/sections/about";
-import { CTASection } from "@/components/sections/cta";
 
 export default function AboutPage() {
     return (
         <>
             <AboutSection />
-            <CTASection />
         </>
     );
 }
